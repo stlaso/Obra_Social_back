7ae21b4fa31016e2913bff1aaafba160905dbd77
Nahuel <nahuelsoriap@gmail.com>
2024-08-15 01:22:41 -0300

    agregado el CRUD para seccionales

--- app/Http/Controllers/SeccionalController.php
+++ app/Http/Controllers/SeccionalController.php
@@ -5,6 +5,9 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Services\SeccionalService;
 use App\Http\Resources\SeccionalResource;
+use App\Http\Requests\SeccionalRequest;
+use App\Exceptions\CustomizeException;
+use Illuminate\Http\Response;
 
 class SeccionalController extends Controller
 {
@@ -17,7 +20,83 @@ class SeccionalController extends Controller
 
     public function index()
     {
-        $seccional=$this->seccionalService->seccionalLista();
+        $seccional = $this->seccionalService->seccionalLista();
         return SeccionalResource::collection($seccional);
+    }
+
+    public function store(SeccionalRequest $request)
+    {
+        try {
+            $validated = $request->validated();
+            $seccional = $this->seccionalService->crearSeccional($validated);
+
+            return response()->json([
+                'message' => 'Seccional creado exitosamente',
+                'data' => new SeccionalResource($seccional),
+            ], 201);
+
+        } catch (\Exception $e) {
+            throw new CustomizeException($e->getMessage(), Response::HTTP_INTERNAL_SERVER_ERROR, $e);
+        }
+    }
+
+    public function update(SeccionalRequest $request, $seccionalId)
+    {
+        try {
+            $validated = $request->validated();
+            $seccional = $this->seccionalService->actualizarSeccional($seccionalId, $validated);
+
+            if (!$seccional) {
+                return response()->json([
+                    'message' => 'Seccional no encontrado',
+                ], 404);
+            }
+
+            return response()->json([
+                'message' => 'Seccional actualizado exitosamente',
+                'data' => new SeccionalResource($seccional),
+            ], 200);
+
+        } catch (\Exception $e) {
+            throw new CustomizeException($e->getMessage(), Response::HTTP_INTERNAL_SERVER_ERROR, $e);
+        }
+    }
+
+    public function show($id)
+    {
+        $seccional = $this->seccionalService->eliminarSeccional($id);
+        return new SeccionalResource($seccional);
+    }
+
+    public function buscarSeccional(Request $request)
+    {
+        try {
+            $query = $request->input('query');
+            $seccionales = $this->seccionalService->buscarSeccional($query);
+    
+            return SeccionalResource::collection($seccionales);
+        } catch (\Exception $e) {
+            throw new CustomizeException('Seccional no encontrada', Response::HTTP_NOT_FOUND);
+        }
+    }    
+
+    public function destroy($id)
+    {
+        try {
+            $seccional = $this->seccionalService->eliminarSeccional($id);
+
+            if (!$seccional) {
+                return response()->json([
+                    'message' => 'Seccional no encontrada',
+                ], 404);
+            }
+
+            return response()->json([
+                'message' => 'Seccional eliminado exitosamente',
+            ], 200);
+
+        } catch (\Exception $e) {
+            throw new CustomizeException('No se pudo eliminar el seccional', Response::HTTP_INTERNAL_SERVER_ERROR);
+        }
     }
 }
--- app/Services/SeccionalService.php
+++ app/Services/SeccionalService.php
@@ -1,13 +1,59 @@
 <?php
 
 namespace App\Services;
+
 use App\Models\Seccional;
 
 class SeccionalService
 {
     public function SeccionalLista()
     {
-        $Seccional=Seccional::orderBy('nombre', 'asc')->get();
+        $Seccional = Seccional::orderBy('nombre', 'asc')->paginate(15);
         return $Seccional;
+    }
+
+    public function crearSeccional($data)
+    {
+        $seccional = Seccional::create([
+            'nombre' => $data['nombre'],
+        ]);
+
+        return $seccional;
+    }
+
+    public function actualizarSeccional($id, $data)
+    {
+        $seccional = Seccional::find($id);
+
+        if (!$seccional) {
+            return null;
+        }
+
+        $seccional->nombre = $data['nombre'] ?? $seccional->nombre;
+
+        $seccional->save();
+
+        return $seccional;
+    }
+
+    public function eliminarSeccional($id)
+    {
+        $seccional = Seccional::find($id);
+
+        if (!$seccional) {
+            return null;
+        }
+
+        $seccional->delete();
+
+        return $seccional;
+    }
+
+    public function buscarSeccional($query)
+    {
+        $seccionales = Seccional::where('nombre', 'LIKE', "%$query%")
+            ->get();
+    
+        return $seccionales;
     }
 }
